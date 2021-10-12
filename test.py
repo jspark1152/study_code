431598733d3aaead5ef43a70de5f17e600656d67
jspark1152 <jspark1152@gmail.com>
2021-10-12 17:49:17 +0900

    1012 리뷰

--- test.py
+++ test.py
@@ -1,15 +1,2 @@
-list1 = list(map(int, input().split()))
 
-def quick_sort(list):
-    if len(list)<=1:
-        return list
-    
-    pivot = list[0]
-    tail = list[1:]
-    x = [i for i in tail if i <= pivot]
-    y = [i for i in tail if i > pivot]
-    return quick_sort(x)+[pivot]+quick_sort(y)
 
-list1 = quick_sort(list1)
-
-print(list1)
