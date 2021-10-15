0ec7259174860e7037cf0fe2eedba2bff3d68f4d
jspark1152 <jspark1152@gmail.com>
2021-10-15 17:26:37 +0900

    수정

--- test.py
+++ test.py
@@ -1,12 +1,43 @@
-def find_parent(parent, x):
-    if parent[x] != x:
-        parent[x] = find_parent(parent, parent[x])
-    return parent[x]
+from collections import deque
 
-def union_parent(parent, a, b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
-    if a < b:
-        parent[b] = a
-    else:
-        parent[a] = b
+print('노드 n, 간선 m')
+n, m = map(int, input().split())
+
+graph = []
+print('간선 정보')
+for i in range(m):
+    a, b = map(int, input().split())
+    graph.append((a, b))
+
+ent = [0] * (n+1)
+
+for i in graph:
+    ent[i[1]] += 1
+
+visited = [False] * (n+1)
+map = []
+
+def top_sort(graph):
+    q=deque()
+    for i in range(1, n+1):
+        if ent[i] == 0:
+            q.append(i)
+            visited[i] = True
+    
+    while q:
+        a = q.popleft()
+        map.append(a)
+        graph = [i for i in graph if i[0] != a]
+
+        link = [0] * (n+1)
+        for i in graph:
+            link[i[1]] += 1
+        
+        for i in range(1, n+1):
+            if link[i] == 0 and not visited[i]:
+                q.append(i)
+                visited[i] = True
+
+top_sort(graph)
+print(map)
+
