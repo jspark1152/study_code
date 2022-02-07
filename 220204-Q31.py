755319dadeb1027b5dd8f8e6f370ad2db37d7c8e
jspark1152 <jspark1152@gmail.com>
2022-02-07 16:56:47 +0900

    0207 머리가 굳은게 분명하다...

--- 220204-Q31.py
+++ 220204-Q31.py
@@ -14,37 +14,43 @@ n X m 크기의 금광
 출력 조건 : 테스트 케이스마다 채굴자가 얻을 수 있는 금의 최대 크기 출력
 '''
 
-print('테스트 케이스 개수 T 입력')
-T = int(input())
+#접근 자체를 잘못한듯 하다
+#하.. 역시 머리가 굳은게 분명하다
 
-case = []
-
-print('테스트 케이스 정보 입력')
-for i in range(T):
+print('테스트 케이스 수 입력 :')
+for tc in range(int(input())):
+    print('금광 정보 입력')
     n, m = map(int, input().split())
-    info = list(map(int, input().split()))
-    case.append(((n, m), info))
-
-print(case)
-
-def max_count(case):
-    map = []
-    n, m = case[0][0], case[0][1]
+    array = list(map(int, input().split()))
+    
+    dp = []
+    index = 0
     for i in range(n):
-        line = []
-        for j in range(m):
-            line.append(case[1][m*i+j])
-        map.append(line)
+        dp.append(array[index:index + m]) #2차원 테이블 초기화
+        index += m
     
-    max = 0
+    for j in range(1, m):
+        for i in range(n):
+            #왼쪽 위에서 dp[i][j]로 접근하는 경우
+            if i == 0: #왼쪽 위에서 오는 경우가 없음
+                left_up = 0
+            else:
+                left_up = dp[i-1][j-1]
+            
+            #왼쪽 아래에서 dp[i][j]로 접근하는 경우
+            if i == n-1: #
+                left_down = 0
+            else:
+                left_down = dp[i+1][j-1]
+            
+            #왼쪽에서 바로 dp[i][j]로 접근하는 경우
+            left = dp[i][j-1]
+            
+            #dp[i][j]의 값을 그 지점에서의 최대값으로 초기화
+            dp[i][j] = dp[i][j] + max(left_up, left_down, left)
     
+    result = 0
     for i in range(n):
-        count = map[i][0]
-        for j in range(1, m):
-            
-            
-        
+        result = max(result, dp[i][m-1])
     
-    return
-
-print(max_count(case[0]))
+    print(result)
