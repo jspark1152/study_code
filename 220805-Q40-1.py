6ad36c398180062b57248534c16d21d489d2c8ae
jspark1152 <jspark1152@gmail.com>
2022-08-05 17:04:38 +0900

    0805 test

---
+++ 220805-Q40-1.py
@@ -0,0 +1,36 @@
+import heapq
+
+inf = int(1e9)
+
+n, m = map(int, input().split())
+
+start = 1
+
+graph = [[] for _ in range(n+1)]
+
+distance = [inf] * (n+1)
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append((b, 1))
+    graph[b].append((a, 1))
+    
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        
+        if distance[now] < dist:
+            continue
+        
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra(start)
+
+print(distance)
